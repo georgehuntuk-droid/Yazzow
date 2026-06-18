5920389cbdd4e387556755d7c3c265fefe09a536
georgehuntuk-droid <georgehuntuk-droid@users.noreply.github.com>
2026-06-18 18:30:42 +0300

    design: update brand logo assets to the new purple logo and update loading screen theme color

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -36,8 +36,8 @@ export default function GlobalLoading() {
         </div>
 
         {/* Sleek Line Loading Indicator */}
-        <div className="w-40 h-1 bg-[#446152]/10 dark:bg-slate-800 rounded-full overflow-hidden">
-          <div className="h-full bg-gradient-to-r from-[#446152] to-[#5d7d6c] rounded-full w-1/2 animate-[loading_1.5s_infinite_ease-in-out]" />
+        <div className="w-40 h-1 bg-[#4437a4]/10 dark:bg-slate-800 rounded-full overflow-hidden">
+          <div className="h-full bg-gradient-to-r from-[#4437a4] to-[#6356c9] rounded-full w-1/2 animate-[loading_1.5s_infinite_ease-in-out]" />
         </div>
       </div>
 
